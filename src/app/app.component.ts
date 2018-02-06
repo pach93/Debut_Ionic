daea4c0001b3c41e34ad029d0d3ba3bfef84b47e
pach93 <dergui2006@hotmail.com*>
2018-02-06 17:18:35 +0000

    deploiement android création de pages navigation entre pages avec menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
+import { MeteoPage } from './../pages/meteo/meteo';
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { GalleryPage } from '../pages/gallery/gallery';
+import { PlacesPage } from '../pages/places/places';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
+
+  menu =[
+    {title:"My Gallery", component : GalleryPage},
+    {title:"My Meteo", component : MeteoPage},
+    {title:"My Places", component : PlacesPage},
+    {title:"My Home", component : HomePage},
+  ];
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -17,6 +27,10 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  onPage(m){
+    this.rootPage = m.component;
   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { PlacesPage } from './../pages/places/places';
+import { MeteoPage } from './../pages/meteo/meteo';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -6,11 +8,12 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { GalleryPage } from '../pages/gallery/gallery';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage, GalleryPage, MeteoPage, PlacesPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +22,7 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage, GalleryPage, MeteoPage, PlacesPage
   ],
   providers: [
     StatusBar,
